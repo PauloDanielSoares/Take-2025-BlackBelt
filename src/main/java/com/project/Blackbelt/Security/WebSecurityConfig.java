3da4bfe0718c91285523fae45e33daad712af58e
Poopstoop1 <daniel.026@hotmail.com>
2025-06-03 02:00:52 -0300

    Correção security

--- src/main/java/com/project/Blackbelt/Security/WebSecurityConfig.java
+++ src/main/java/com/project/Blackbelt/Security/WebSecurityConfig.java
@@ -2,16 +2,14 @@ package com.project.Blackbelt.Security;
 
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -20,23 +18,14 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
-
 @Configuration
 @EnableWebMvc
 public class WebSecurityConfig implements WebMvcConfigurer {
 
-	@Autowired
 	private final SecurityDatabaseService securityDatabaseService;
 
 	public WebSecurityConfig(SecurityDatabaseService securityDatabaseService) {
 		this.securityDatabaseService = securityDatabaseService;
-	}
-
-	@Autowired
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(securityDatabaseService).passwordEncoder(NoOpPasswordEncoder.getInstance());
 	}
 	
 	@Override
@@ -72,24 +61,14 @@ public class WebSecurityConfig implements WebMvcConfigurer {
 	}
 	
 	@Bean
-	public UserDetailsService userDetailsService() {
-		return securityDatabaseService;
+	public PasswordEncoder passwordEncoder() {
+		
+		return NoOpPasswordEncoder.getInstance();
 	}
-	/*
-	 * @Bean public InMemoryUserDetailsManager userDetailsService() {
-	 * 
-	 * @SuppressWarnings("deprecation") UserDetails user =
-	 * User.withDefaultPasswordEncoder().username("user").password("password").roles
-	 * ("USER") .build();
-	 * 
-	 * @SuppressWarnings("deprecation") UserDetails user2 =
-	 * User.withDefaultPasswordEncoder().username("user2").password("1234567").roles
-	 * ("USER") .build();
-	 * 
-	 * @SuppressWarnings("deprecation") UserDetails admin =
-	 * User.withDefaultPasswordEncoder().username("admin").password("12345678").
-	 * roles("MANAGER") .build(); return new InMemoryUserDetailsManager(user, user2,
-	 * admin); }
-	 */
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
 
 }
